6829416e02a059c151858eac50fd64c592103322
Deepraj Podder <deeprajpodder7@gmail.com>
2022-08-09 17:16:04 +0530

    Add files via upload

---
+++ mathOperation.java
@@ -0,0 +1,16 @@
+class mathOperation{
+public static void main(String args[]){
+System.out.println("Enter two numbers:");
+int a = 18;
+int b = 6;
+System.out.println(a + " " + b);
+int sum= a+b;
+int product= a*b;
+int division= a/b;
+int subtract= a-b;
+System.out.println("The sum is :" + sum);
+System.out.println("The product is :" + product);
+System.out.println("The divided result is :" + division);
+System.out.println("The subtracted result is :" + subtract);
+}
+}
