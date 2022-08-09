5b42271a607b75e668589aa0769317849689712e
Deepraj Podder <deeprajpodder7@gmail.com>
2022-08-09 16:25:56 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,11 @@
+class A{
+static int c=10;
+int a= 10;
+public static void main(String args[]){
+A obj = new A();
+int b=20;
+System.out.println(A.c);
+System.out.println(b);
+System.out.println(obj.a);
+}
+}
