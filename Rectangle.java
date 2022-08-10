6d8df0a7110a235a2a90f71c5690e2cd6ce1c2ff
Deepraj Podder <deeprajpodder7@gmail.com>
2022-08-10 15:20:18 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+class Rectangle{
+public static void main(String args[]){
+int a = 5;
+int b= 4;
+System.out.print("The length is:");
+System.out.println(a);
+System.out.print("The breadth is:");
+System.out.println(b);
+int area= a*b;
+int perimeter= 2*(b+a);
+System.out.println("The area is :" + area);
+System.out.println("The perimeter is :" + perimeter);
+}
+}
---
+++ Square.java
@@ -0,0 +1,11 @@
+class Square{
+public static void main(String args[]){
+System.out.print("The side is:");
+int a = 5;
+System.out.println(a);
+int area= a*a;
+int perimeter= 4*a;
+System.out.println("The area is :" + area);
+System.out.println("The perimeter is :" + perimeter);
+}
+}
