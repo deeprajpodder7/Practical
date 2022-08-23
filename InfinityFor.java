1eee83185f02873034bcab02175d1281a958c7ce
Deepraj Podder <deeprajpodder7@gmail.com>
2022-08-23 17:13:17 +0530

    Add files via upload

---
+++ InfinityFor.java
@@ -0,0 +1,12 @@
+package forLoop;
+
+public class InfinityFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(;;) {
+	System.out.println("Need more practice");
+}
+	}
+
+}
