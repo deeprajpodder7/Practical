e9630f77fd172c807463e763c02c5487e754c8ec
Deepraj Podder <deeprajpodder7@gmail.com>
2022-08-23 16:58:42 +0530

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,14 @@
+package forLoop;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//declaring array
+		int arr[] = {1,2,3,4,5};
+		for (int i: arr) {
+			System.out.println(i);
+		}
+	}
+
+}
