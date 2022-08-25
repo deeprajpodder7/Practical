ab6278e6c1197f9f470c0dc0972be38f9e7986d8
Deepraj Podder <deeprajpodder7@gmail.com>
2022-08-25 15:11:43 +0530

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,15 @@
+package WhileDoWhile;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=2;
+System.out.println("First ten even numbers:");
+do {
+	System.out.println(i);
+	i+=2;
+}while(i<=10);
+	}
+
+}
---
+++ WhileLoop.java
@@ -0,0 +1,17 @@
+package WhileDoWhile;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*int i=1;
+while(i<=5) {
+	System.out.println(i);
+	i++;*/
+while(true) {
+	System.out.println("Infinity Times");
+	
+	}
+	}
+
+}
