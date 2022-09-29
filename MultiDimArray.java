f38f91ab38f98c0a23b4fee1a691fac45d085a0d
Deepraj Podder <deeprajpodder7@gmail.com>
2022-09-29 12:30:33 +0530

    Create MultiDimArray.java

---
+++ MultiDimArray.java
@@ -0,0 +1,14 @@
+public class MulDArray1 { 
+public static void main(String args[]){  
+//declaring and initializing 2D array  
+//int arr[][]={{1,2,3},{2,4,5},{4,4,5}};  
+	int arr[][]={{1,2},{2,4},{4,4}}; 
+//printing 2D array  
+for(int i=0;i<3;i++){  
+// for(int j=0;j<3;j++){  
+	 for(int j=0;j<2;j++){ 
+   System.out.print(arr[i][j]+" ");  
+ }  
+ System.out.println();  
+}  
+}}  
